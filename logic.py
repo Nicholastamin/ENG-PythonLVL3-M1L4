a2717338762067ee1897e70f09dbb86c0bea3b83
Nicholastamin <nickevan0110@gmail.com>
2025-08-03 01:07:26 -0700

    p

--- logic.py
+++ logic.py
@@ -8,6 +8,8 @@ class Pokemon:
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
+        self.base = None
+
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
         else:
@@ -28,7 +30,29 @@ class Pokemon:
         # A method that returns information about the pokémon
         if not self.name:
             self.name = await self.get_name()  # Retrieving a name if it has not yet been uploaded
-        return f"The name of your Pokémon: {self.name}"  # Returning the string with the Pokémon's name
+            self.base = await self.show_base()
+
+
+        return f"The name of your Pokémon: {self.name}\n base {self.name}"  # Returning the string with the Pokémon's name
 
     async def show_img(self):
         # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['sprites']['other']['showdown']['front_shiny']  # Returning a Pokémon's name
+                else:
+                    return "image not found"  # Return the default name if the request fails
+
+    async def show_base(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['base_experience']  # Returning a Pokémon's name
+                else:
+                    return "base not found"  # Return the default name if the request fails
